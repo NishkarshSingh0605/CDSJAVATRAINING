b3686c2a2d586cceb1d8c6e4f23b1a7156bea598
Nishkarsh Singh <nishkarshsingh95@gmail.com>
2024-07-24 21:26:20 +0530

    Swapwithouttemp.java

--- Swapping.java
+++ Swapping.java
@@ -1,13 +1,13 @@
-public class Swapping {
-    public static void main(String[] args) {
-        int x = 1;
-        int y = 2;
+class Swapping {
 
-        System.out.println("Before swapping: x = " + x + " y = " + y);
-        int temp = x;
-        x = y;
-        y = temp;
-
-        System.out.println("After swapping: x = " + x + " y = " + y);
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 5;
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("After swapping:"
+                + " x = " + x + ", y = " + y);
     }
 }
